ada6187a3d5b5190c8aba2c03ea12167049f0fb5
Alexander Hück <hueck.alexander@gmail.com>
2023-11-05 14:49:52 +0100

    Mock tests

--- test/pass/c/heap_milc_struct_mock.c
+++ test/pass/c/heap_milc_struct_mock.c
@@ -1,4 +1,5 @@
 // RUN: %c-to-llvm %s | %apply-verifier 2>&1 | %filecheck %s
+// RUN: %c-to-llvm %s | %opt -O2 -S | %apply-verifier 2>&1 | %filecheck %s
 
 #include <stdlib.h>
 
@@ -25,10 +26,11 @@ typedef struct gather_t {
   int offset_increment;
 } gather_t;
 
-static gather_t* gather_array;
+gather_t* gather_array;
 
 void foo(int sites_on_node, int dir) {
   // CHECK: Extracted Type: {{.*}} = !DIDerivedType(tag: DW_TAG_pointer_type, baseType: [[DIREF:![0-9]+]], size: 64)
   // CHECK: Final Type: [[DIREF]] = !DIBasicType(name: "int", size: 32, encoding: DW_ATE_signed)
+  // CHECK-NEXT: Pointer level: 1
   gather_array[dir].neighbor = (int*)malloc(sites_on_node * sizeof(int));
 }
