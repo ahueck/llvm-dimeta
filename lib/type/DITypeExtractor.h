c9bf4db540dd586e81636cae5989c0e037f5d371
Alexander Hück <hueck.alexander@gmail.com>
2024-10-06 23:11:08 +0200

    Refactoring

---
+++ lib/type/DITypeExtractor.h
@@ -0,0 +1,27 @@
+//  Dimeta library
+//  Copyright (c) 2022-2023 Alexander Hück
+//  Distributed under the BSD 3-Clause license.
+//  (See accompanying file LICENSE)
+//  SPDX-License-Identifier: BSD-3-Clause
+//
+
+#ifndef DIMETA_DITYPEEXTRACTOR_H
+#define DIMETA_DITYPEEXTRACTOR_H
+
+#include <optional>
+
+namespace dimeta::dataflow {
+struct ValuePath;
+}  // namespace dimeta::dataflow
+
+namespace llvm {
+class DIType;
+}
+
+namespace dimeta::type {
+
+std::optional<llvm::DIType*> find_type(const dataflow::ValuePath& path);
+
+}
+
+#endif
